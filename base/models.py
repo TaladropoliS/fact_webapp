a706982d4b0667ac0859070fcbb0b835b90b252c
TaladropoliS <fernando.taladriz@gmail.com>
2021-11-05 12:00:30 -0300

    app inv 01

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True, verbose_name='Estado')
+    fc = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    fm = models.DateTimeField(auto_now=True, verbose_name='Modificado')
+    uc = models.ForeignKey(User, related_name='usuario_crea', on_delete=models.CASCADE)
+    um = models.ForeignKey(User, related_name='usuario_modifica', on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
--- webapp/settings.py
+++ webapp/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #   mis APPs
     'base.apps.BaseConfig',
+    'inv.apps.InvConfig',
 ]
 
 MIDDLEWARE = [
